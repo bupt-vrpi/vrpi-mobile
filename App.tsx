5e2a5437a381ba46ce89fc5d23d3b0f9f2d46a3f
YXL <chenxin.lan.76@gmail.com>
2020-10-02 23:40:56 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { askAsync, MOTION } from "expo-permissions";
+import { Accelerometer, Gyroscope, ThreeAxisMeasurement } from "expo-sensors";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+async function getPermission() {
+  const { status } = await askAsync(MOTION);
+  if (status === "granted") {
+    if (!(await Accelerometer.isAvailableAsync())) {
+      throw new Error("Accelerometer sensor is not available");
+    }
+    if (!(await Gyroscope.isAvailableAsync())) {
+      throw new Error("Gyroscope sensor is not available");
+    }
+  } else {
+    throw new Error("Motion permission not granted");
+  }
+}
 
 export default function App() {
+  const [available, setAvailable] = useState(false);
+  const [updateInterval, setUpdateInterval] = useState(1000);
+  const [accelerometerData, setAccelerometerData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  } as ThreeAxisMeasurement);
+  const [gyroscopeData, setGyroscopeData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  } as ThreeAxisMeasurement);
+
+  const _setUpdateInterval = (interval: number) => {
+    Accelerometer.setUpdateInterval(interval);
+    Gyroscope.setUpdateInterval(interval);
+  };
+
+  getPermission()
+    .then(() => {
+      setAvailable(true);
+
+      Accelerometer.addListener((data) => {
+        setAccelerometerData(data);
+      });
+      Gyroscope.addListener((data) => {
+        setGyroscopeData(data);
+      });
+      _setUpdateInterval(updateInterval);
+    })
+    .catch(() => {});
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>UpdateInterval: {updateInterval}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          setUpdateInterval((oldValue) => oldValue + 100);
+          _setUpdateInterval(updateInterval);
+        }}
+      >
+        <Text>Increase</Text>
+      </TouchableOpacity>{" "}
+      <TouchableOpacity
+        onPress={() => {
+          setUpdateInterval(
+            (oldValue) => oldValue - (oldValue > 100 ? 100 : 0)
+          );
+          _setUpdateInterval(updateInterval);
+        }}
+      >
+        <Text>Decrease</Text>
+      </TouchableOpacity>
+      {available && (
+        <View>
+          <Text style={styles.text}>Accelerometer:</Text>
+          <Text style={styles.text}>{accelerometerData.x}</Text>
+          <Text style={styles.text}>{accelerometerData.y}</Text>
+          <Text style={styles.text}>{accelerometerData.z}</Text>
+          <Text style={styles.text}>Gyroscope:</Text>
+          <Text style={styles.text}>{gyroscopeData.x}</Text>
+          <Text style={styles.text}>{gyroscopeData.y}</Text>
+          <Text style={styles.text}>{gyroscopeData.z}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -12,8 +90,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
   },
 });
