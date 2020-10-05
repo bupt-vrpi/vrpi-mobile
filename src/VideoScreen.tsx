d1f4ed9a33cff5c946407eb83d9c57a63d2717ba
YXL <chenxin.lan.76@gmail.com>
2020-10-05 19:59:17 +0800

    🐳chore(dependencies): add `expo-av`

--- src/VideoScreen.tsx
+++ src/VideoScreen.tsx
@@ -1,10 +1,19 @@
+import { Video } from "expo-av";
 import React from "react";
-import { View, Text } from "react-native";
 
 export default function VideoScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Video</Text>
-    </View>
+    <Video
+      source={{
+        uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+      }}
+      rate={1.0}
+      volume={1.0}
+      isMuted={false}
+      resizeMode="cover"
+      shouldPlay
+      isLooping
+      style={{ width: 300, height: 300 }}
+    />
   );
 }
